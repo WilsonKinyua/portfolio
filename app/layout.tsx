1d893fd010ecdd6718ed5e1ed59ea2ae0c8bb211
Wilson Kinyua <wilsonkinyuam@gmail.com>
2024-05-03 13:56:21 +0300

    style(seo): Enhance SEO metadata with description and social media tags. (SEO) Improved website metadata for better search engine visibility and social media sharing. Added favicon for improved site identification and navigation. Created robots.txt and sitemap.xml for search engine crawling and indexing.

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ import { montserrat } from "@/lib/fonts";
 
 export const metadata: Metadata = {
   title: "Wilson Kinyua | Software Engineer",
-  description: "Software Engineer | Lead Developer | Head of Tech | UI/UX Designer | Technical Trainer/Mentor | Frontend Engineer | Backend Engineer",
+  description: "Driven, innovative Software Engineer with experience in the development of software and solutions. A conscientious person who pays attention to details. Very passionate about software development, always willing and ready to learn new things/concepts. Proven leader with the ability to streamline development processes to drive the achievement of organizational objectives. An ambitious and generally curious professional who strives for greatness and is driven to grow himself and those around oneself through collaboration.",
 };
 
 export default function RootLayout({
@@ -22,7 +22,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
+      <head>
+        <title>Wilson Kinyua | Software Engineer</title>
+        <meta name="title" content="Wilson Kinyua | Software Engineer" />
+        <meta name="description" content="Driven, innovative Software Engineer with experience in the development of software and solutions. A conscientious person who pays attention to details. Very passionate about software development, always willing and ready to learn new things/concepts. Proven leader with the ability to streamline development processes to drive the achievement of organizational objectives. An ambitious and generally curious professional who strives for greatness and is driven to grow himself and those around oneself through collaboration." />
+        <meta name="keywords" content="Software Engineer | Lead Developer | Head of Tech | UI/UX Designer | Technical Trainer/Mentor | Frontend Engineer | Backend Engineer" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.wilsonkinyua.com/" />
+        <meta property="og:title" content="Wilson Kinyua | Software Engineer" />
+        <meta property="og:description" content="Driven, innovative Software Engineer with experience in the development of software and solutions. A conscientious person who pays attention to details. Very passionate about software development, always willing and ready to learn new things/concepts. Proven leader with the ability to streamline development processes to drive the achievement of organizational objectives. An ambitious and generally curious professional who strives for greatness and is driven to grow himself and those around oneself through collaboration." />
+        <meta property="og:image" content="/img/me.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.wilsonkinyua.com/" />
+        <meta property="twitter:title" content="Wilson Kinyua | Software Engineer" />
+        <meta property="twitter:description" content="Driven, innovative Software Engineer with experience in the development of software and solutions. A conscientious person who pays attention to details. Very passionate about software development, always willing and ready to learn new things/concepts. Proven leader with the ability to streamline development processes to drive the achievement of organizational objectives. An ambitious and generally curious professional who strives for greatness and is driven to grow himself and those around oneself through collaboration." />
+        <meta property="twitter:image" content="/img/me.png" />
+
+        <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon" />
+
+      </head>
       <body
         className={cn(montserrat.className)}
       >
--- lib/actions.ts
+++ lib/actions.ts
@@ -6,7 +6,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (name: string, email: string, message: string, subject?: string, phoneNumber?: string) => {
     const { data, error } = await resend.emails.send({
-        from: 'My Portfolio 😃 <wilson@wdev.co.ke>',
+        from: 'My Portfolio 😃 <website@wilsonkinyua.com>',
         to: ["wilsonkinyuam@gmail.com"],
         subject: subject || `New Message from ${name} on My Portfolio`,
         html: `
